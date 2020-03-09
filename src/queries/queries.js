842332476f6a3edbc3788b924f7382fd06bb19a6
Marisa DeMeglio <marisa.demeglio@gmail.com>
2020-03-08 23:37:03 -0700

    UI tweak for results editing page

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -352,6 +352,7 @@ module.exports = {
             testBook {
                 title
                 version
+                filename
                 lang {
                     id
                     label
