1460a7e17a9e4edb99ad7cc2ed57f2f2757d7af4
Marisa DeMeglio <marisa.demeglio@gmail.com>
2020-11-07 22:13:31 -0800

    Edit user invite email text

--- src/emails.js
+++ src/emails.js
@@ -1,54 +1,5 @@
 module.exports = {
-    reinvite: {
-        subject:'Your account on epubtest.org',
-        text: linkTokenUrl => `
-Greetings!
-
-This is a message from the DAISY Consortium, to invite you to continue your participation in
-Reading System Accessibility Testing at the newly redesigned epubtest.org.
-
-We've just redesigned our website to be more streamlined and accessibility-focused, and we'd like to 
-let you know that your account has been transferred to the new site.
-
-To complete this process, paste this link into your browser and set a password for your account:
-${linkTokenUrl}.
-
-Then login with this email address and your new password, and set up your profile.
-
-Don't hesitate to contact us at epubtest@daisy.org with any questions.
-
-Thanks,
-
-The DAISY Consortium
-daisy.org
-epubtest.org
-inclusivepublishing.org
-`,
-
-        html: linkTokenUrl => `
-<p>Greetings!</p>
-
-<p>This is a message from the <a href="http://daisy.org">DAISY Consortium</a>, to invite you to continue your participation in
-Reading System Accessibility Testing at the newly redesigned <a href="http://epubtest.org">epubtest.org</a>.
-
-<p>We've just redesigned our website to be more streamlined and accessibility-focused, and we'd like to 
-let you know that your account has been transferred to the new site.</p>
-
-<p>To complete this process, <a href="${linkTokenUrl}">accept this invitation</a></p> and set a password for your account.
-
-<p>Don't hesitate to <a href="mailto:epubtest@daisy.org">contact us</a> with any questions.</p>
-
-<p>Thanks,
-<br/>
-<span>The DAISY Consortium</span>
-<br/>
-<a href="http://daisy.org">daisy.org</a>
-<br/>
-<a href="http://epubtest.org">epubtest.org</a>
-<br/>
-<a href="http://inclusivepublishing.org">inclusivepublishing.org</a>
-`},
-
+    
     invite: {
         subject: 'Invitation to participate in epubtest.org',
         text: linkTokenUrl => `
@@ -58,11 +9,11 @@ This is a message from the DAISY Consortium, to invite you to participate in
 Reading System Accessibility Testing at epubtest.org.
 
 To accept this invitation, paste this link into your browser:
-${linkTokenUrl}.
+${linkTokenUrl}
 
-Don't hesitate to contact us at epubtest@daisy.org with any questions.
+Don't hesitate to write back with any questions!
 
-Thanks,
+Best regards,
 
 The DAISY Consortium
 daisy.org
@@ -76,11 +27,11 @@ inclusivepublishing.org
 <p>This is a message from the <a href="http://daisy.org">DAISY Consortium</a>, to invite you to participate in
 Reading System Accessibility Testing at <a href="http://epubtest.org">epubtest.org</a>.
 
-<p><a href="${linkTokenUrl}">Accept this invitation</a></p>
+<p><a href="${linkTokenUrl}">Accept this invitation</a> to proceed.</p>
 
-<p>Don't hesitate to <a href="mailto:epubtest@daisy.org">contact us</a> with any questions.</p>
+<p>Don't hesitate to write back with any questions!</p>
 
-<p>Thanks,
+<p>Best regards,
 <br/>
 <span>The DAISY Consortium</span>
 <br/>
@@ -100,11 +51,11 @@ Hello,
 Someone has requested a password reset for this account on epubtest.org. If it was not you, then please disregard this message.
 
 To reset your password, paste this link into your browser:
-${linkTokenUrl}.
+${linkTokenUrl}
 
-Don't hesitate to contact us at epubtest@daisy.org with any questions.
+Don't hesitate to write back with any questions.
 
-Thanks,
+Best regards,
 
 The DAISY Consortium
 daisy.org
@@ -120,9 +71,9 @@ If it was not you, then please disregard this message.</p>
 
 <p><a href="${linkTokenUrl}">Reset your password</a></p>
 
-<p>Don't hesitate to <a href="mailto:epubtest@daisy.org">contact us</a> with any questions.</p>
+<p>Don't hesitate to write back with any questions.</p>
 
-<p>Thanks,
+<p>Best regards,
 <br/>
 <span>The DAISY Consortium</span>
 <br/>
