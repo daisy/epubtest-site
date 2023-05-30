b4fd70d79f68a4e4b4a9cb900fa058a9fa476c5b
Marisa DeMeglio <marisa.demeglio@gmail.com>
2023-05-30 16:14:01 -0700

    new query: get all testing environments for a subset of test books
    typically the test books would all belong to the same topic. this gets the latest answer set for a topic; what is "latest" may vary among RS due to frequency of test answer updates

--- src/queries/testingEnvironmentsWithAnswers.js
+++ src/queries/testingEnvironmentsWithAnswers.js
@@ -1,5 +1,6 @@
 import generate from './crudGenerator.js';
 import * as testEnvs from './testingEnvironments.js';
+import * as answerSets from './answerSets.js';
 
 const {CREATE, DELETE, UPDATE, GET, GET_ALL} 
     = generate("testingEnvironment", "testingEnvironments", testEnvs.FIELDS_WITH_ANSWERS)
@@ -29,8 +30,18 @@ query($userId: Int!) {
         ${testEnvs.FIELDS_WITH_ANSWERS()}
     }
 }`;
+
+// get each testing environment's latest answer public answer set for test books in a topic (each topic has one test book but there can be many versions)
+// also get all the answers
+const GET_ALL_BY_TESTBOOKS = () => `
+query($testBookIds: [Int!]) {
+    testingEnvironments(condition: {isPublic: true, isArchived: false}) {
+        ${testEnvs.BASIC_FIELDS()}
+        answerSets (condition: {isLatestPublic: true}, filter: { testBookId: {in: $testBookIds} } ) {
+            ${answerSets.FIELDS_WITH_ANSWERS()}
+        }
     }
 }`;
 
 export {
-    CREATE, DELETE, UPDATE, GET, GET_PUBLISHED, GET_ALL, GET_ALL_PUBLISHED, GET_ALL_ARCHIVED, GET_ALL_BY_USER};
+    CREATE, DELETE, UPDATE, GET, GET_PUBLISHED, GET_ALL, GET_ALL_PUBLISHED, GET_ALL_ARCHIVED, GET_ALL_BY_USER, GET_ALL_BY_TESTBOOKS};
