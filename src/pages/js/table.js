2b919226876f18c630efe09727002cdc726df3d8
Marisa DeMeglio <marisa.demeglio@gmail.com>
2025-11-01 22:08:34 -0700

    simplify table URL search params
    for #116

--- src/pages/js/table.js
+++ src/pages/js/table.js
@@ -4,7 +4,7 @@ Turns an HTML table into one with sorting, filtering, and searching
 Depends on dayjs if you want to sort by date
 */
 // import * as TableState from './tableState.js';
-import * as TableState from './tableStateQP.js';
+// import * as TableState from './tableStateQP.js';
 // sorting utility functions
 let sortHelpers = {
     "alpha": (row1, row2, path) => {
@@ -41,6 +41,32 @@ let docurl = () => {
     let url = new URL(document.URL);
     return `${url.protocol}//${url.host}${url.pathname}`;
 };
+// sortStr is formatted as index-dir eg 3-a or 0-d
+let parseSort = sortStr => {
+    if (!sortStr || sortStr == '' || !sortStr.split(['-']) || sortStr.split(['-']).length != 2) {
+        return null;
+    }
+    let index = sortStr.split(['-'])[0];
+    let dir = sortStr.split(['-'])[1] == 'a' ? 'ascending' : 'descending';
+    return {index, dir};
+}
+let setURLSearchParam = (id, value) => {
+    let url = new URL(window.location.href);
+    url.searchParams.set(id.trim(), value.trim());
+    window.history.pushState(null, '', url.toString());
+};
+    
+let getURLSearchParam = id => {
+    let url = new URL(window.location.href);
+    return url.searchParams.get(id.trim());
+};
+
+let clearURLSearchParam = id => {
+    let url = new URL(window.location.href);
+    url.searchParams.delete(id.trim());
+    window.history.pushState(null, '', url.toString());
+};
+let rowOrRows = length => length == 1 ? 'row' : 'rows';
 
 class EnhancedTable  {
     
@@ -59,24 +85,40 @@ class EnhancedTable  {
         localStorage.removeItem(docurl());
     }
 
+    ID = (str) => `${str}${this.idString}`;
+
+    // URL string values
+    /* 
+    search=string
+    sort=index-dir e.g. 1-a or 4-d
+    filterid=value
+    */
+
     // get values from the URL
-    getSearchText = () => TableState.getSearchText(docurl(), this.tableElm.id);
-    getSort = () => TableState.getSort(docurl(), this.tableElm.id);
-    getFilters = () => TableState.getFilters(docurl(), this.tableElm.id);
-    clearSort = () => TableState.clearSort(docurl(), this.tableElm.id);
-    setSortIndex = idx => TableState.storeSortIndex(docurl(), this.tableElm.id, idx);
-    setSortDir = dir => TableState.storeSortDir(docurl(), this.tableElm.id, dir);
-    setFilter = (filterId, value) => TableState.storeFilterValue(docurl(), this.tableElm.id, filterId, value);
+    getSearchText = () => getURLSearchParam(this.ID('search')) || '';
+    getSort = () => getURLSearchParam(this.ID('sort'));
+    getFilter = (filterId) => getURLSearchParam(filterId);
+    clearSort = () => clearURLSearchParam(this.ID('sort'));
+    setSort = (index, dir) => setURLSearchParam(this.ID('sort'), `${index}-${dir == "ascending" ? 'a' : 'd'}`);
+    setFilter = (filterId, value) => {
+        if (value == 'All') {
+            clearURLSearchParam(filterId);
+        }
+        else {
+            setURLSearchParam(filterId, value);
+        }
+    }
+    
     setInitFilterValue = (filterId, value) => {
-        let filters = TableState.getFilters(docurl(), this.tableElm.id);
         // only set the value if the filter doesn't already have an entry
-        if (filters.length > 0) {
-            if (!filters.find(f => f.id == filterId)) {
-                this.setFilter(filterId, value);
-            }
+        if (getURLSearchParam(filterId) == null && value != 'All') {
+            setURLSearchParam(filterId, value);
         }
     };
-    setSearchText = str => TableState.storeSearchText(docurl(), this.tableElm.id, str);
+    setSearchText = str => str != '' ? setURLSearchParam(this.ID('search'), str) : clearURLSearchParam(this.ID('search'));
+
+    
+
     clearFilters = () => {
         let filters = this.getFilters();
         filters.map(f => this.setFilter(f.id, "All"));
@@ -86,21 +128,21 @@ class EnhancedTable  {
         this.controlsElm.innerHTML = `
         <fieldset class="table-filters hidden">
             <legend class="visually-hidden">Filters</legend>
-            <div id="table-search${this.idString}" class="hidden">
-                <label for="table-search-input${this.idString}">Search</label>
-                <input type="text" id="table-search-input${this.idString}" name="table-search-input">
+            <div id="${this.ID('search-container')}" class="hidden">
+                <label for="${this.ID('search')}">Search</label>
+                <input type="text" id="${this.ID('search')}" value="${this.getSearchText() || '' }">
             </div>
-            <button id="reset${this.idString}" class="hidden">Reset</button>
-            <span class="rowcount" id="rowcount${this.idString}"><span>${this.tableElm.querySelectorAll('tbody tr').length}</span> rows shown.</span>
+            <button id="${this.ID('reset')}" class="hidden">Reset</button>
+            <span class="rowcount" id="rowcount${this.idString}"><span>${this.tableElm.querySelectorAll('tbody tr').length}</span> ${rowOrRows(this.tableElm.querySelectorAll('tbody tr').length)} shown.</span>
         </fieldset>`;
-        this.controlsElm.querySelector(`#table-search-input${this.idString}`).addEventListener("keyup", e => {
+        this.controlsElm.querySelector(`#${this.ID('search')}`).addEventListener("keyup", e => {
             if (e.keyCode == 27) { // escape
                 e.target.value = '';
             }
             this.setSearchText(e.target.value);
             this.refreshTable();
         });
-        this.controlsElm.querySelector(`#reset${this.idString}`).addEventListener("click", e => this.reset());
+        this.controlsElm.querySelector(`#${this.ID('reset')}`).addEventListener("click", e => this.reset());
     }
     
     /* sortRules = [{
@@ -142,8 +184,7 @@ class EnhancedTable  {
                             newSortDirValue = "descending";
                         }
                     }
-                    this.setSortIndex(idx);
-                    this.setSortDir(newSortDirValue);
+                    this.setSort(idx, newSortDirValue);
                     this.refreshTable();
                 });
             }
@@ -158,7 +199,7 @@ class EnhancedTable  {
     enableFilters(filterRules) {
         this.filterRules = filterRules;
         this.controlsElm.querySelector("fieldset").classList.remove("hidden");
-        this.controlsElm.querySelector(`#reset${this.idString}`).classList.remove("hidden");
+        this.controlsElm.querySelector(`#${this.ID('reset')}`).classList.remove("hidden");
         this.filterRules.map((filterRule, idx) => {
 
             // all the possible values
@@ -185,19 +226,24 @@ class EnhancedTable  {
                 options.push("None");
             }
             let newFilter = document.createElement("div");
-            let filterId = `filter-${idx}-${this.idString}`;
+            // make the filterId from the filter label plus the table ID string if present
+            let filterId = `${filterRule.label.toLowerCase().replaceAll(' ', '-')}${this.idString}`;//`filter-${idx}-${this.idString}`;
             newFilter.innerHTML = `
             <label for="${filterId}">${filterRule.label}</label>
             <select id="${filterId}">
-                ${options.map(option => `<option value="${option}">${option}</option>`).join('')}
+                ${options.map(option => `<option 
+                    value="${option}" 
+                    ${this.getFilter(filterId) && this.getFilter(filterId) == option ? 'selected' : ''}>
+                ${option}</option>`).join('')}
             </select>
             `;
-            this.setInitFilterValue(filterId, "All"); 
+            this.setInitFilterValue(filterId, this.getFilter(filterId) || "All"); 
             this.controlsElm.querySelector("fieldset").insertBefore(newFilter, 
-                this.controlsElm.querySelector(`#table-search${this.idString}`));
+                this.controlsElm.querySelector(`#${this.ID('search-container')}`));
             
         }).join('');
-        
+        this.refreshTable();
+
         Array.from(this.controlsElm.querySelectorAll("select")).map(selectElm => selectElm.addEventListener("change", e => {
             this.setFilter(selectElm.id, e.target.value);
             this.refreshTable();
@@ -206,8 +252,8 @@ class EnhancedTable  {
     
     enableSearch() {
         this.controlsElm.querySelector("fieldset").classList.remove("hidden");
-        this.controlsElm.querySelector(`#reset${this.idString}`).classList.remove("hidden");
-        this.controlsElm.querySelector(`#table-search${this.idString}`).classList.remove('hidden');
+        this.controlsElm.querySelector(`#${this.ID('reset')}`).classList.remove("hidden");
+        this.controlsElm.querySelector(`#${this.ID('search-container')}`).classList.remove('hidden');
     }
     filterRows(rows) {
         let selectElms = Array.from(this.controlsElm.querySelectorAll("select"));
@@ -228,7 +274,11 @@ class EnhancedTable  {
         if (this.sortRules.length == 0) {
             return rows;
         }
-        let sortRule = this.sortRules.find(sr => sr.columnIndex == this.getSort().index);
+        let sortParam = parseSort(this.getSort());
+        if (!sortParam) {
+            return rows;
+        }
+        let sortRule = this.sortRules.find(sr => sr.columnIndex == sortParam.index);
         if (!sortRule) {
             return rows;
         }
@@ -237,7 +287,7 @@ class EnhancedTable  {
         let sortFn = sortHelpers[sortRule.type];
         tableRows.sort((a, b) => sortFn(a, b, sortRule.pathFn));
         
-        if (this.getSort().dir == "descending") {
+        if (sortParam.dir == "descending") {
             tableRows = tableRows.reverse();
         }
         return tableRows;
@@ -259,10 +309,13 @@ class EnhancedTable  {
         let columnHeaders = Array.from(this.tableElm.querySelectorAll("thead th"));
         // remove aria-sort from all headers
         columnHeaders.map(ch => ch.removeAttribute("aria-sort"));
-        let sortIndex = this.getSort().index;
-        if (sortIndex > 0 && sortIndex < columnHeaders.length) {
-            let columnHeader = columnHeaders[sortIndex];
-            columnHeader.setAttribute("aria-sort", this.getSort().dir);
+        let sortParam = parseSort(this.getSort());
+        if (sortParam) {
+            let sortIndex = sortParam.index;
+            if (sortIndex > 0 && sortIndex < columnHeaders.length) {
+                let columnHeader = columnHeaders[sortIndex];
+                columnHeader.setAttribute("aria-sort", sortParam.dir);
+            }
         }
 
         let tbody = this.tableElm.querySelector("tbody");
@@ -279,7 +332,10 @@ class EnhancedTable  {
         let size = this.getSize();
         this.tableElm.setAttribute("aria-colcount", size.columns);
         this.tableElm.setAttribute("aria-rowcount", size.rows);
-        this.controlsElm.querySelector(`#rowcount${this.idString} span`).textContent = size.rows;
+        this.controlsElm.querySelector(`#rowcount${this.idString}`).innerHTML = `<span class="rowcount" id="rowcount${this.idString}">
+            <span>${size.rows}</span> 
+            ${rowOrRows(size.rows)} shown.
+        </span>`;
 
         let filteredDescription = ": filtered view";
         let baseTitle = document.title.replaceAll(filteredDescription, '');
@@ -292,8 +348,8 @@ class EnhancedTable  {
     }
     reset() {
         this.tableElm.querySelector("tbody").innerHTML = this.originalTBodyInnerHTML;
-        this.controlsElm.querySelector(`#table-search-input${this.idString}`).value = '';
-        this.setSearchText('');
+        this.controlsElm.querySelector(`#${this.ID('search')}`).value = '';
+        clearURLSearchParam(this.ID('search'));
         Array.from(this.controlsElm.querySelectorAll("select")).map(selectElm => {
             selectElm.value = "All";
             this.setFilter(selectElm.id, "All");
@@ -306,34 +362,28 @@ class EnhancedTable  {
         let rows = this.tableElm.querySelectorAll("tbody > tr").length;
         return {columns, rows};
     }
-    loadFromStore() {
-        const entries = performance.getEntriesByType("navigation");
+    // // the "store" at the moment is just the URL search params
+    // // it used to be localstorage hence some of the function names
+    // loadFromStore() {
         
-        // use the stored values as input to the UI controls
-        if (this.getSearchText()) {
-            this.searchText = this.getSearchText();
-            this.controlsElm.querySelector(`#table-search-input${this.idString}`).value = this.searchText;
-        }
-        if (this.getSort()) {
-            this.sortByIndex = this.getSort().index;
-            this.sortByDir = this.getSort().dir;
-        }
-        if (this.getFilters()) {
-            let filters = this.getFilters();
-            filters.map(filter => {
-                let select = this.controlsElm.querySelector("#" + filter.id);
-                if (select) {
-                    let options = Array.from(select.querySelectorAll('option'));
-                    let option = options?.find(opt => opt.value == filter.value);
-                    if (option) {
-                        option.selected = true;
-                    }
-                }
-            });
-        }
-        this.refreshTable();
+    //     if (this.getFilters()) {
+    //         let filters = this.getFilters();
+    //         filters.map(filter => {
+    //             let select = this.controlsElm.querySelector("#" + filter.id);
+    //             if (select) {
+    //                 let options = Array.from(select.querySelectorAll('option'));
+    //                 let option = options?.find(opt => opt.value == filter.value);
+    //                 if (option) {
+    //                     option.selected = true;
+    //                 }
+    //             }
+    //         });
+    //     }
+    //     this.refreshTable();
+    // }
+    loadFromStore() {
+
     }
-    
 }
 
 export { EnhancedTable };
